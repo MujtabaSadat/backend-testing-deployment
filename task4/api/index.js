39f5ac161905f620afe4fd138d94dab444a5ec6c
mujtaba sadat <ag1545@student.jamk.fi>
2026-04-16 21:33:46 +0300

    added task4 exercise

--- task4/api/index.js
+++ task4/api/index.js
@@ -1,6 +1,5 @@
 const app = require("../app");
-const albumRoutes = require("./routes/album");
-
+const albumRoutes = require("../routes/album");
 app.use("/albums", albumRoutes);
 
 module.exports = app;
