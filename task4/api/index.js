b311cecaeb366dd790c8f575a60c29ddf0a3d3bf
mujtaba sadat <ag1545@student.jamk.fi>
2026-04-16 22:08:17 +0300

    added task4 exercise

--- task4/api/index.js
+++ task4/api/index.js
@@ -1,24 +1,20 @@
 const mongoose = require("mongoose");
 const app = require("../app");
-const albumRoutes = require("../routes/album");
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.use("/albums", albumRoutes);
 
 let isConnected = false;
 
-async function connectToDatabase() {
+async function connectDB() {
   if (isConnected) return;
 
-  await mongoose.connect(process.env.MONGO_URI);
-  isConnected = true;
-  console.log("MongoDB connected");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("DB error:", err);
+  }
 }
 
-// connect once per cold start
-connectToDatabase();
+connectDB();
 
 module.exports = app;
--- task4/app.js
+++ task4/app.js
@@ -1,17 +1,16 @@
-// app.js
 const express = require("express");
-require("./db");
 const albumRoutes = require("./routes/album");
 
 const app = express();
+
 app.use(express.json());
 
+// Home route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// API routes
 app.use("/albums", albumRoutes);
 
-const PORT = process.env.PORT || 3000;
-if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
 module.exports = app;
